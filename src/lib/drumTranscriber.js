6f7b3150254e9b64373657353b55ed224b083d3b
VitukoLesPaul <vituko.lespaul@gmail.com>
2026-09-25 13:46:11 +0000

    fix: eliminar linea EOF colada en drumTranscriber.js

--- src/lib/drumTranscriber.js
+++ src/lib/drumTranscriber.js
@@ -218,7 +218,7 @@ function normalizeAmplitude(onsetEnergy, allEnergies, options) {
 function onsetsAreSimultaneous(a, b, windowSec) {
   return Math.abs(a.timeSeconds - b.timeSeconds) <= windowSec;
 }
-EOF
+
 // ---------------------------------------------------------------------------
 // API principal
 // ---------------------------------------------------------------------------
